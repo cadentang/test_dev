200480fc90f71494c6eae08099af2a2242910719
cadentang <623260178@qq.com>
2018-09-21 23:48:49 +0800

    提交登录初步功能

---
+++ test_platform/user_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+	return render(request, "index.html")
+
+
+def login(request):
+
+	if request.method =='GET':
+		username = request.GET.get("username")
+		password = request.GET.get("password")
+
+		if username == "" or password == "":
+			return render(request, "index.html", {"error": "用户名或密码不能为空!"})
+
+
+
